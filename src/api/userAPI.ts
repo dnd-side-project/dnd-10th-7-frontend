f4c77b99624b72e8ab5beb253d87bae2255af5c8
chamny20 <dge3179@ajou.ac.kr>
2024-02-08 16:28:21 +0900

    🚚 Chore(#11): modified comment

--- src/api/userAPI.ts
+++ src/api/userAPI.ts
@@ -9,13 +9,14 @@ export type userInfoType = {
 };
 
 export const userAPI = {
-  //닉네임 중복
+  // 닉네임 중복
   getDuplicatedNickname: (nickname: string) => {
     return api.get(`/api/users/check`, {
       params: nickname,
     });
   },
 
+  // 유저 정보 추가 입력
   postUserInfo: (data: userInfoType) => {
     return api.post(`/api/users/sign-up`, {
       nickname: data.nickname,
