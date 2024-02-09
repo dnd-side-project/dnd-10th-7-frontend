3df58bdabe9ecc70d5d00e42ca0c281612770b63
seoyoung81 <seosue0801@gmail.com>
2024-02-09 17:24:56 +0900

    📍 Feat(#15): project top ui

---
+++ src/app/project/register/page.tsx
@@ -0,0 +1,12 @@
+import RegisterProjectTop from "./RegisterProjectTop"
+
+export default function RegisterProject () {
+    return (
+        <div className="w-[1440px] flex flex-col">
+
+            {/* 프로젝트 등록 상단 */}
+            <RegisterProjectTop />
+
+        </div>
+    )
+}
