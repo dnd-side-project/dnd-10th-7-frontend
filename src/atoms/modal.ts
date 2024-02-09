fc159b24dfa637146cc2e5c79cdc42699b44dcdf
chamny20 <chamny20@naver.com>
2024-02-10 03:15:23 +0900

    📍 Feat(#11): add recoil atom

--- src/atoms/modal.ts
+++ src/atoms/modal.ts
@@ -1,19 +1,35 @@
 import { atom } from "recoil";
 
-export const nicknameModal = atom<string>({
+export const nicknameState = atom<string>({
   key: `nicknameModal`,
   default: "",
 });
-export const userInfoModal = atom<Record<string, string>>({
-  key: `userInfoModal`,
-  default: {
-    birth: "",
-    gender: "",
-    career: "",
-  },
+
+export const birthState = atom<string>({
+  key: `birthState`,
+  default: "",
 });
 
-export const interestModal = atom<string>({
-  key: `interestModal`,
+export const genderState = atom<string>({
+  key: `genderState`,
   default: "",
+});
+
+export const careerState = atom<string>({
+  key: `careerState`,
+  default: "",
+});
+
+// export const userInfoModal = atom<Record<string, string>>({
+//   key: `userInfoModal`,
+//   default: {
+//     birth: "",
+//     gender: "",
+//     career: "",
+//   },
+// });
+
+export const interestState = atom<Array<string>>({
+  key: `interestState`,
+  default: [],
 });
