e00f84c2374a35bd58dbcb35e570aeb42382fecf
chamny20 <chamny20@naver.com>
2024-02-09 22:21:06 +0900

    📍 Feat(#11): add nickname-check logic

--- src/components/sign-up/NicknameModal.tsx
+++ src/components/sign-up/NicknameModal.tsx
@@ -1,14 +1,20 @@
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useMemo, useState } from "react";
 import Modal from "../common-components/modal";
 import { ModalViewProps } from "./LoginModal";
 import { GrayInput } from "../common-components/input";
 import Button from "../common-components/button";
 import UserInfoModal from "./UserInfoModal";
 import { userAPI } from "@component/api/userAPI";
+import clsx from "clsx";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import { useRecoilState } from "recoil";
+import { nicknameModal } from "@component/atoms/modal";
 
 export default function NicknameModal(props: ModalViewProps) {
   const { isOpen, setIsOpen } = props;
   const [nickname, setNickname] = useState<string>("");
+  // const [nickname, setNickname] = useRecoilState(nicknameModal);
+  const [nickInvalid, setNickInvalid] = useState<boolean>(true);
 
   // 다음 모달을 위한 state
   const [infoModal, setInfoModal] = useState<boolean>(false);
@@ -26,15 +32,31 @@ export default function NicknameModal(props: ModalViewProps) {
   };
 
   const handleClick = () => {
-    // next step
     console.log("clicked next-step");
-    setIsOpen(true); // 현재 모달 닫고
-    // 이 사이에 닉네임 값 저장하기
+    setIsOpen(false);
 
     if (typeof window !== "undefined")
       localStorage.setItem("nickname", nickname);
 
-    setInfoModal(true); // 다음 모달 열기
+    setInfoModal(true);
+  };
+
+  const checkInvalid = () => {
+    // invalid check
+    if (nickname.length > 8) {
+      setNickInvalid(false);
+    } else {
+      setNickInvalid(true);
+    }
+  };
+
+  useEffect(() => {
+    checkInvalid();
+  }, [nickname]);
+
+  const handleClose = () => {
+    setIsOpen(false);
+    setNickname("");
   };
 
   return (
@@ -44,7 +66,7 @@ export default function NicknameModal(props: ModalViewProps) {
         onClose={() => setIsOpen(false)}
         // className="w-[480px]"
       >
-        <Modal.Close onClick={() => setIsOpen(false)} />
+        <Modal.Close onClick={handleClose} />
         <Modal.Title>
           <div>닉네임을 입력해주세요</div>
         </Modal.Title>
@@ -53,28 +75,62 @@ export default function NicknameModal(props: ModalViewProps) {
         </Modal.SubTitle>
 
         <Modal.Description className="flex flex-col gap-[11px]">
-          <div>
+          <div className="relative">
             <GrayInput
               value={nickname}
               onChange={(e) => setNickname(e.target.value)}
               placeholder="닉네임을 입력해주세요."
               size="xs"
+              className={clsx(
+                !nickInvalid &&
+                  "caret-error-main focus:ring-0 focus:outline-error-main"
+              )}
             />
-            <button onClick={checkNickname}>중복확인</button>
+            <button
+              onClick={checkNickname}
+              className="absolute right-[16px] top-[9px] h-[32px] rounded-[5px] p-[9px] bg-[#EEEEEE] text-gray-80 text-caption1 items-center flex justify-center text-center"
+            >
+              중복확인
+            </button>
+            <div className="pt-[8px] text-caption1 text-gray-60 flex flex-row justify-between">
+              {!nickInvalid ? (
+                <p className="text-error-main">
+                  한글, 영어 (2~8자) 이내로 입력해주세요.
+                </p>
+              ) : nickname.length === 0 ? (
+                <p>한글, 영어 (2~8자) 이내로 입력해주세요.</p>
+              ) : (
+                <div className="flex flex-row items-center gap-[4px] text-purple-main1 text-caption1">
+                  <CheckCircleOutlineIcon className="text-body2" />
+                  <p>사용 가능한 닉네임이에요.</p>
+                </div>
+              )}
+              <p>({nickname.length}/8)</p>
+            </div>
           </div>
         </Modal.Description>
 
         <Modal.Footer className="flex flex-row gap-[8px]">
-          <Button size="md" color="gray">
+          {/* <Button size="md" color="gray">
             이전으로
-          </Button>
-          <Button size="md" color="default" onClick={handleClick}>
+          </Button> */}
+          <Button
+            size="md"
+            color="default"
+            onClick={handleClick}
+            disabled={nickname.length > 8 || nickname.length === 0}
+          >
             선택완료
           </Button>
         </Modal.Footer>
       </Modal>
 
-      <UserInfoModal isOpen={infoModal} setIsOpen={setInfoModal} />
+      <UserInfoModal
+        isOpen={infoModal}
+        setIsOpen={setInfoModal}
+        isPrevOpen={isOpen}
+        setIsPrevOpen={setIsOpen}
+      />
     </>
   );
 }
