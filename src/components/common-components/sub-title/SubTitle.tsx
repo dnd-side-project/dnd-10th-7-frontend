21b395bbfd5aa748dc82e406bbd8de3517e05784
chamny20 <chamny20@naver.com>
2024-02-15 02:16:10 +0900

    📍 Feat(#26): add sub-title component

---
+++ src/components/common-components/sub-title/SubTitle.tsx
@@ -0,0 +1,29 @@
+import clsx from "clsx";
+import { PropsWithChildren, forwardRef } from "react";
+
+export type SubTitleProps = {
+  title: string;
+  className?: string;
+};
+
+const SubTitle = forwardRef<HTMLDivElement, PropsWithChildren<SubTitleProps>>(
+  ({ children, title, className }, ref) => {
+    return (
+      <div
+        className={clsx(
+          "w-3/5 max-w-[600px] flex flex-col gap-4 py-[8px] text-title text-gray-80"
+        )}
+        ref={ref}
+      >
+        {title}
+        <div className="flex flex-col gap-3 text-body1 font-normal">
+          {children}
+        </div>
+      </div>
+    );
+  }
+);
+
+SubTitle.displayName = "SubTitle";
+
+export default SubTitle;
