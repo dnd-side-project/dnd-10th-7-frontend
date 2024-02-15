5513ef60f6e30436b0cd238faad457df48f27022
seoyoung81 <seosue0801@gmail.com>
2024-02-15 21:26:02 +0900

    📍 Feat(#38): add like adn scrap button

--- src/app/project/[projectId]/page.tsx
+++ src/app/project/[projectId]/page.tsx
@@ -1,9 +1,13 @@
+"use client";
 import ProjectSendbackTitleData from "@component/components/project/ProjectSendbackTitleData";
 import ProjectSendbackUserInfo from "@component/components/project/ProjectSendbackUserInfo";
 import Button from "@component/components/common-components/button/Button";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 import Link from "next/link";
 import ProjectDetailImage from "./ProjectDetailImage";
+import { useState } from "react";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
 
 type PageParams = {
   projectId: number;
@@ -39,6 +43,38 @@ export default function ProjectDetailPage({ params }: { params: PageParams }) {
     createdAt: "2022.05.05 11:22",
     endedAt: "2022.05.10 11:22",
   };
+
+  const [likeCount, setLikeCount] = useState<number>(projectData.likeCount);
+  // TODO: api 업데이트 되면 default 값도 response 값으로 변경
+  const [likeState, setLikeState] = useState<boolean>(false);
+  const [scrappedCount, setScrappedCount] = useState<number>(
+    projectData.scrappedCount
+  );
+  const [scrapState, setScrapState] = useState<boolean>(false);
+
+  const handleLikeClick = () => {
+    // 좋아요 버튼 클릭 시
+    if (likeCount === projectData.likeCount) {
+      setLikeCount(likeCount + 1);
+      setLikeState(true)
+      // 해제 시
+    } else {
+      setLikeCount(projectData.likeCount);
+      setLikeState(false)
+    }
+  };
+
+  const handleScrappedClick = () => {
+    // 스크랩 버튼 클릭 시
+    if (scrappedCount === projectData.scrappedCount) {
+      setScrappedCount(scrappedCount + 1);
+      setScrapState(true)
+    } else {
+      setScrappedCount(projectData.scrappedCount);
+      setScrapState(false)
+    }
+  };
+
   return (
     <div className="w-[1440px] flex flex-col items-center">
       <section className="max-w-[1080px] w-full mt-[56.5px]">
@@ -66,15 +102,25 @@ export default function ProjectDetailPage({ params }: { params: PageParams }) {
         </Link>
 
         {/* 이미지 */}
-        <ProjectDetailImage
-          projectImgUrl={projectData.projectImgUrl}
-        />
+        <ProjectDetailImage projectImgUrl={projectData.projectImgUrl} />
 
         {/* 기간 */}
         <div className="min-w-[787px] mt-[72px]">기간</div>
 
         {/* 멤버 */}
         <div className="min-w-[787px] mt-[72px]">멤버</div>
+
+        {/* 좋아요, 스크랩 */}
+        <div className="mt-[72px]">
+          <Button color={likeState ? "border" : "grayBorder"} className="me-5" onClick={handleLikeClick}>
+            <ThumbUpIcon className="me-2" />
+            <span className="text-body1 font-medium">{likeCount}</span>
+          </Button>
+          <Button color={scrapState ? "border" : "grayBorder"} onClick={handleScrappedClick}>
+            <BookmarkIcon className="me-2" />
+            <span className="text-body1 font-medium">{scrappedCount}</span>
+          </Button>
+        </div>
       </section>
     </div>
   );
