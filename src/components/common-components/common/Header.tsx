9eba0c81dfa07bc542cbf397ce999ef8e44b8894
chamny20 <chamny20@naver.com>
2024-02-18 17:19:01 +0900

    📍 Feat(#42): apply dropbox to header

--- src/components/common-components/common/Header.tsx
+++ src/components/common-components/common/Header.tsx
@@ -3,6 +3,8 @@
 import clsx from "clsx";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import DropdownBox from "../dropdown-box";
 
 export const Header = () => {
   const variants = {
@@ -11,6 +13,8 @@ export const Header = () => {
 
   const username = "chamny";
   const router = useRouter();
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   return (
     <div className="sticky z-50 top-0 m-auto py-[17px] w-full h-[70px] flex flex-row items-center justify-center bg-white text-h2">
@@ -34,8 +38,8 @@ export const Header = () => {
             프로젝트 등록하기
           </div>
         </div>
-
-        {!localStorage.getItem("accessToken") ? (
+        {/* !localStorage.getItem("accessToken") */}
+        {false ? (
           <div
             className={clsx(variants.menu)}
             onClick={() => router.push("/login")}
@@ -43,15 +47,26 @@ export const Header = () => {
             로그인
           </div>
         ) : (
-          <div className="flex flex-row items-center gap-2">
-            {/* <Image
+          <div className="relative">
+            <div
+              className={clsx(
+                variants.menu,
+                "flex flex-row items-center gap-2"
+              )}
+              onClick={() => setIsOpen((prev) => !prev)}
+            >
+              {/* <Image
           src={profileImg}
           alt={username}
           width={60}
           height={60}
           className="w-[34px] h-[34px] rounded-full me-8"
         ></Image> */}
-            <div>{username}</div>
+              <div>{username}</div>
+            </div>
+            {isOpen && (
+              <DropdownBox items={["마이페이지", "로그아웃"]} place="right" />
+            )}
           </div>
         )}
       </div>
