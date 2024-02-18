c7f60eaf3517f77b7da92c619ee82f5dd86dba3f
chamny20 <chamny20@naver.com>
2024-02-19 03:05:17 +0900

    ✂️ Remove(#1): remove unneeded files

--- src/app/api/googleGetAPI.ts
+++
@@ -1,9 +0,0 @@
-import { api } from "./api";
-
-export const GoogleLogInAPI = {
-  getGoogleLogIn: (code: string) => {
-    return api.get(`/api/auth/google/callback`, {
-      params: { code },
-    });
-  },
-};
