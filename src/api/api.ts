97f2f6fe67321e856b2d10c178a6d12a35aa98ba
chamny20 <chamny20@naver.com>
2024-02-19 03:06:27 +0900

    📍 Feat(#1): modified api

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,29 +3,34 @@ import axios, { AxiosInstance } from "axios";
 // env에 BASE_URL 설정 필요
 // 인증 값이 필요 없는 경우
 export const api = axios.create({
-    baseURL: process.env.BASE_URL,
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL: process.env.BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
 // 인증 값이 필요한 경우
-export const authApi = ({ ContentType }: {ContentType: string}) => {
-    const headers: Record<string, string> = {
-        'Content-Type': ContentType,
-    }
-    
-    // token
-    const accessToken: string | null = typeof window !== "undefined" ? sessionStorage.getItem("accessToken") : "error";
-    
-    if (accessToken) {
-        headers.accessToken = `${accessToken}`
-    };
+export const authApi = ({ ContentType }: { ContentType: string }) => {
+  const headers: Record<string, string> = {
+    "Content-Type": ContentType,
+  };
 
-    const instance: AxiosInstance = axios.create({
-        baseURL: process.env.BASE_URL,
-        headers
-    })
+  // token
+  const accessToken: string | null =
+    typeof window !== "undefined"
+      ? sessionStorage.getItem("accessToken")
+      : "error";
 
-    return instance; 
-}
+  if (accessToken) {
+    headers.accessToken = `${accessToken}`;
+  }
+
+  const instance: AxiosInstance = axios.create({
+    baseURL: process.env.BASE_URL,
+    headers,
+  });
+
+  return instance;
+};
