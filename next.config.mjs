5350b4bf845cd0770350f268726260f73c493a46
seoyoung81 <seosue0801@gmail.com>
2024-02-19 15:29:41 +0900

    📍 Feat(#49): s3 image upload to next

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // 외부 이미지 불러오기
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: `k.kakaocdn.net`,
+      },
+      
+    ],
+  },
+};
 
 export default nextConfig;
