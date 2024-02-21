3fbe0d101e7099ad1d2e411b23cd90e95049628c
chamny20 <dge3179@ajou.ac.kr>
2024-02-21 12:04:12 +0900

    📍 Feat(#62): apply api

--- src/components/comment/CommentBox.tsx
+++ src/components/comment/CommentBox.tsx
@@ -1,7 +1,39 @@
+import { useGetProjectComment } from "@component/hooks/useProject";
 import { CommentInput } from "./CommentInput";
 import { CommentItem } from "./CommentItem";
+import { useMemo } from "react";
+import { ProjectCommentType } from "@component/types/Project";
 
-export default function CommentBox() {
+type PageParams = {
+  projectId: number;
+};
+
+export default function CommentBox({ params }: { params: PageParams }) {
+  const {
+    data: commentData,
+    error,
+    isLoading,
+  } = useGetProjectComment(params.projectId);
+
+  const commentList = useMemo(
+    () =>
+      commentData?.data?.map(
+        (item: ProjectCommentType) =>
+          ({
+            userId: item.userId,
+            nickname: item.nickname,
+            profileImageUrl: item.profileImageUrl,
+            commentId: item.commentId,
+            content: item.content,
+            createdAt: item.createdAt,
+            isAuthor: item.isAuthor,
+          }) as ProjectCommentType
+      ) ?? [],
+    [commentData?.data]
+  );
+
+  console.log("댓글 리스트들", commentList);
+
   return (
     <div className="w-full max-w-[800px]">
       <div className="pb-[37px] flex items-center gap-3">
@@ -9,7 +41,7 @@ export default function CommentBox() {
         <p className="text-h2 text-gray-60">32</p>
       </div>
       <CommentInput />
-      <CommentItem />
+      <CommentItem data={commentList} />
     </div>
   );
 }
--- src/components/comment/CommentItem.tsx
+++ src/components/comment/CommentItem.tsx
@@ -1,62 +1,15 @@
 import { useState } from "react";
 import { ReplyComment } from "./ReplyComment";
+import { ProjectCommentType } from "@component/types/Project";
+import Image from "next/image";
 
-export const CommentItem = () => {
-  const dummyComment = [
-    {
-      commentId: 1,
-      user: "chaeminenie",
-      nickname: "cjcjcjcj",
-      profileImageUrl: "aiaiaiaiaiaiaai",
-      createdDate: "2024.24.24",
-      time: "2시간전",
-      content:
-        "굉장히 흥미로운 서비스 어쩌구 졸리다 지금 새벽 4시다지금 새벽 4시다지금 새벽 4시다지금 새벽 4시다",
-    },
-    {
-      commentId: 2,
-      user: "chaeminenie",
-      nickname: "cjcjcjcj",
-      profileImageUrl: "aiaiaiaiaiaiaai",
-      createdDate: "2024.24.24",
-      time: "2시간전",
-      content:
-        "굉장히 흥미로운 서비스 어쩌구 졸리다 지금 새벽 4시다지금 새벽 4시다지금 새벽 4시다지금 새벽 4시다",
-    },
-    {
-      commentId: 3,
-      user: "chaeminenie",
-      nickname: "cjcjcjcj",
-      profileImageUrl: "aiaiaiaiaiaiaai",
-      createdDate: "2024.24.24",
-      time: "2시간전",
-      content:
-        "굉장히 흥미로운 서비스 어쩌구 졸리다 지금 새벽 4시다지금 새벽 4시다지금 새벽 4시다지금 새벽 4시다",
-    },
-    {
-      commentId: 4,
-      user: "chaeminenie",
-      nickname: "cjcjcjcj",
-      profileImageUrl: "aiaiaiaiaiaiaai",
-      createdDate: "2024.24.24",
-      time: "2시간전",
-      content:
-        "굉장히 흥미로운 서비스 어쩌구 졸리다 지금 새벽 4시다지금 새벽 4시다지금 새벽 4시다지금 새벽 4시다",
-    },
-    {
-      commentId: 5,
-      user: "chaeminenie",
-      nickname: "cjcjcjcj",
-      profileImageUrl: "aiaiaiaiaiaiaai",
-      createdDate: "2024.24.24",
-      time: "2시간전",
-      content:
-        "굉장히 흥미로운 서비스 어쩌구 졸리다 지금 새벽 4시다지금 새벽 4시다지금 새벽 4시다지금 새벽 4시다",
-    },
-  ];
+export type Props = {
+  data: ProjectCommentType[];
+};
 
+export const CommentItem = ({ data }: Props) => {
   const [openReplyComments, setOpenReplyComments] = useState<Array<boolean>>(
-    new Array(dummyComment.length).fill(false)
+    new Array(data.length).fill(false)
   );
 
   const toggleReplyComment = (index: number) => {
@@ -67,21 +20,26 @@ export const CommentItem = () => {
 
   return (
     <div className="flex flex-col space-y-8">
-      {dummyComment.map((item, idx) => {
+      {data.map((item, idx) => {
         return (
           <div key={idx}>
             <div className="flex gap-[25px] items-center">
-              {/* profile-img */}
+              <Image
+                src={item.profileImageUrl}
+                alt="profile"
+                // TODO : size 다시 수정 필요
+                width={47}
+                height={47}
+                className="w-[47px] h-[47px] rounded-full me-4 object-cover"
+              ></Image>
               <div className="h-[48px] w-[48px] rounded-full bg-gray-40" />
 
               <div className="flex flex-col space-y-1">
                 <div className="flex gap-[8px] items-center">
-                  <p className="text-body2">{item.user}</p>
-                  <p className="text-caption1 text-gray-60">
-                    {item.createdDate}
-                  </p>
+                  <p className="text-body2">{item.nickname}</p>
+                  <p className="text-caption1 text-gray-60">{item.createdAt}</p>
                   <p className="w-[5px] h-[5px] rounded-full bg-gray-60" />
-                  <p className="text-caption1 text-gray-60">{item.time}</p>
+                  {/* <p className="text-caption1 text-gray-60">{item.time}</p> */}
                   <p className="w-[5px] h-[5px] rounded-full bg-gray-60" />
                   <p
                     className="text-caption1 text-gray-60 cursor-pointer"
