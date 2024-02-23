c8d7382656ab1ccdae3ee35c08f79d1f5c190407
Chaemin Lee <chamny20@naver.com>
2024-02-24 04:38:54 +0900

    최종 레이아웃 수정 (#91)
    * 🔨 Fix: hotfix
    
    * 🔨 Fix: hotfix
    
    * 🔨 Fix: hotfix
    
    * 🔨 Fix: hotfix
    
    * 🔨 Fix: hotfix
    
    * 🔨 Fix: hotfix
    
    * 🔨 Fix: fix project-api
    
    * 🔨 Fix: hotfix
    
    * 🔨 Fix: modified data-fetching type
    
    * 🚚 Chore: modified comment
    
    * 🚚 Chore: modified comment about google
    
    * 🚚 Chore: modified logic about google
    
    * 🔨 Fix: fix layout and add img
    
    * 🔨 Fix: fix
    
    * 🔨 Fix: fix import error
    
    * 🔨 Fix: fix error
    
    * 🔨 Fix: modified ui

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -4,12 +4,9 @@ import { Suspense } from "react";
 export default function ProjectPage() {
   return (
     <Suspense>
-      {/* 컨테이너 너비 */}
-      <section className="m-auto max-w-[1080px] w-full mt-[100px] flex flex-col gap-[100px] mb-[100px]">
-        <Suspense>
-          {/* 프로젝트 */}
-          <ProjectList />
-        </Suspense>
+      <section className="mx-auto max-w-[1080px] w-full mt-[100px] flex flex-col gap-[100px] mb-[100px]">
+        {/* 프로젝트 */}
+        <ProjectList />
       </section>
     </Suspense>
   );
