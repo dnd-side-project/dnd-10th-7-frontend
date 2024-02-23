b30828f7e467f862407707db1b8291ae82d0182b
chamny20 <chamny20@naver.com>
2024-02-24 01:22:12 +0900

    🔨 Fix: modified data-fetching type

--- src/components/landing/project/ProjectList.tsx
+++ src/components/landing/project/ProjectList.tsx
@@ -55,7 +55,7 @@ export const ProjectList = () => {
 
   const projectList = useMemo(
     () =>
-      projectListData?.data?.content.map(
+      projectListData?.data?.data?.content.map(
         (item: ProjectItemProps) =>
           ({
             commentCount: item.commentCount,
