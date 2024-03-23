ee11c3a8db1a5c03bcee8c96c9da389031aa8bf9
seoyoung81 <seosue0801@gmail.com>
2024-03-23 22:56:26 +0900

    🚚 Chore(#104): add header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
         <ThemeProvider theme={theme}>
           <body>
             <Providers>
-         
+              <Header />
               {children}
             </Providers>
             <Footer />
